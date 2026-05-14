9b11e635c2bc56aabd2acf4bf9e11dbefc5831cd
Azuhari <144159803+ADAzhari@users.noreply.github.com>
2026-05-14 15:19:21 +0700

    fixed a few things

--- feature_engineering/landmark_extractor.py
+++ feature_engineering/landmark_extractor.py
@@ -1,24 +1,22 @@
 # feature_engineering/landmark_extractor.py
 import cv2
-import mediapipe as mp
 import numpy as np
+
+# import cara baru yang works di 0.10.x
+import mediapipe as mp
+from mediapipe.python.solutions import face_mesh as mp_face_mesh
 
 class LandmarkExtractor:
     def __init__(self):
-        self.mp_face_mesh = mp.solutions.face_mesh
-        self.face_mesh = self.mp_face_mesh.FaceMesh(
+        self.face_mesh = mp_face_mesh.FaceMesh(
             static_image_mode=False,
             max_num_faces=1,
-            refine_landmarks=True,  # lebih presisi di area mata
+            refine_landmarks=True,
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5
         )
 
     def extract(self, frame):
-        """
-        Input  : frame BGR dari OpenCV
-        Output : array (478, 3) koordinat landmark, atau None
-        """
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = self.face_mesh.process(rgb)
 
@@ -28,7 +26,6 @@ class LandmarkExtractor:
         h, w, _ = frame.shape
         landmarks = results.multi_face_landmarks[0].landmark
 
-        # konversi ke pixel coordinates
         points = np.array([
             (lm.x * w, lm.y * h, lm.z * w)
             for lm in landmarks
